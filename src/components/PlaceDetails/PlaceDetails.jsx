c9c43108353b1c90712b9c1550c8d2c1d9b195e7
Sajib <contact2sajib@gmail.com>
2021-10-28 21:12:58 +0600

    intial

---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PlaceDetails = ({ place }) => {
+    console.log(place)
+    return (
+        <div>
+            <h1>{place.name}</h1>
+        </div>
+    )
+}
+
+export default PlaceDetails
